f621392fc927214e740808d13c80ed8f00db95aa
syed-x-farhan <161956977+syed-x-farhan@users.noreply.github.com>
2025-04-02 19:57:27 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -368,7 +368,7 @@ def scrape_coinmarketcap():
         selenium_logger.setLevel(logging.WARNING)
         
         # Open the CoinMarketCap community page for AAVE
-        url = "https://coinmarketcap.com/community/search/latest/convex-finance/"
+        url = "https://coinmarketcap.com/community/search/latest/balancer/"
         logger.info(f"Opening URL: {url}")
         driver.get(url)
         
