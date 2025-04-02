08d18204c5a1f652cd7f604f59077c722c848a0d
syed-x-farhan <161956977+syed-x-farhan@users.noreply.github.com>
2025-04-02 18:36:51 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -368,7 +368,7 @@ def scrape_coinmarketcap():
         selenium_logger.setLevel(logging.WARNING)
         
         # Open the CoinMarketCap community page for AAVE
-        url = "https://coinmarketcap.com/community/search/latest/aave/"
+        url = "https://coinmarketcap.com/community/search/latest/dydx-chain/"
         logger.info(f"Opening URL: {url}")
         driver.get(url)
         
